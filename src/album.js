15a40cbf5f93bbe329fb3b1908afd747473e93a8
gjlacerda <guilherme.lcd@gmail.com>
2018-02-01 22:10:06 -0200

    Added return promise to all methods

--- src/album.js
+++ src/album.js
@@ -2,16 +2,16 @@ import {API_URL} from './config';
 import {toJSON} from './utils';
 
 export const getAlbum = (id) => {
-    fetch(`${API_URL}/albums/${id}`)
+    return fetch(`${API_URL}/albums/${id}`)
         .then(toJSON);
 };
 
 export const getAlbums = (ids) => {
-    fetch(`${API_URL}/albums/?ids=${ids}`)
+    return fetch(`${API_URL}/albums/?ids=${ids}`)
         .then(toJSON);
 };
 
 export const getAlbumsTracks = (id) => {
-    fetch(`${API_URL}/albums/${id}/tracks`)
+    return fetch(`${API_URL}/albums/${id}/tracks`)
         .then(toJSON);
 };
--- src/search.js
+++ src/search.js
@@ -2,22 +2,22 @@ import {API_URL} from './config';
 import {toJSON} from './utils';
 
 export const search = (query, type) => {
-    fetch(`${API_URL}/search?q=${query}&type=${type}`)
+    return fetch(`${API_URL}/search?q=${query}&type=${type}`)
         .then(toJSON);
 };
 
 export const searchArtists = (query) => {
-    search(query, 'artist');
+    return search(query, 'artist');
 };
 
 export const searchAlbums = (query) => {
-    search(query, 'album');
+    return search(query, 'album');
 };
 
 export const searchTracks = (query) => {
-    search(query, 'track');
+    return search(query, 'track');
 };
 
 export const searchPlaylists = (query) => {
-    search(query, 'playlist');
+    return search(query, 'playlist');
 };
